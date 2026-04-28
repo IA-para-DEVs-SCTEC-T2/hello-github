a8607fbdacef318259fc5fe5dc66dd9ed6c4f0be
Andrea Veronica González <andreavgonzalez7@gmail.com>
2026-04-28 20:07:53 -0300

    add hello_andrea.py and update hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,10 @@ def hello():
     print("Hello Felipe Oliveira")
     print({"Nome": "Felipe Oliveira", "Role": "Professor"})
 
+def hello_andera():
+    print("Hello Andera")
+    print({"Nome": "Andera", "Role": "Student"})
+
 if __name__ == "__main__":
     hello()
+    hello_andera()
---
+++ hello_andrea.py
@@ -0,0 +1,6 @@
+def hello_andrea():
+    print("Hello Andrea")
+    print({"Nome": "Andrea", "Role": "Student"})
+
+if __name__ == "__main__":
+    hello_andrea()
